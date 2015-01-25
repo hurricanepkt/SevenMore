aad4f5a891da110582f3ee6e0dd409fc8f0d9cd5
Mark Greenway <mgreenway@gmail.com>
2015-01-25 15:54:29 -0500

    Day2 Medium

--- lua/Day2/Medium.lua
+++ lua/Day2/Medium.lua
@@ -1,3 +1,4 @@
+dofile("util.lua")
 function concatenate(a1, a2) 
 	local result = {}
 	local iter = 1;
@@ -11,4 +12,57 @@ function concatenate(a1, a2)
 	end
 	return result
 end
+
+local arraymt = {
+	__add = concatenate
+}
+local _priv={}
+local global_mt = {
+	__newindex = function(t, k, v) 
+		setmetatable(v, arraymt)
+		_priv[k]=v
+	end,
+	__index = function(t, k)
+		return _priv[k]
+	end
+}
+setmetatable(_G, global_mt)
+
+b1 = { 1, 2, 3}
+b2 = { 4, 5, 6} 
+
+print(table_to_string(b1 + b2))
+
+local Queue = {}
+Queue.__index = Queue
+
+function Queue.new()
+	local self = setmetatable({}, Queue)
+	local arr  = {}
+	self.ind = 1
+	self.lower = 1
+	self.arr = arr
+	return self
+end
+
+function Queue:add(val)
+	self.arr[self.ind] = val
+	print('added ' .. val .. ' to ' .. self.ind)
+	self.ind = self.ind + 1
+end
+
+function Queue:remove(val)
+	local ret = self.arr[self.lower]
+	self.arr[self.lower] = nil
+	self.lower = self.lower + 1
+	return ret
+end
+
+print ("QUEUE!")
+local q = Queue:new()
+q:add('A')
+q:add('B')
+print(q:remove())
+print(q:remove())
+print(q:remove())
 
